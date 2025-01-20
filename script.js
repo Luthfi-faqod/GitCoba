c7bd6e1571f63a1637cd8a8fc4d08c5e45d00d57
Muhammad Luthfi <faqodupi@gmail.com>
2025-01-20 11:07:28 +0200

    memperbeaiki divide

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const add = (a, b) => a + b;
-console.log(add(2,1));
+console.log(add(3,1));
 
 const substract = (a, b) => a - b;
-console.log(substract(2,1));
+console.log(substract(1,1));
 
 const divide = (a, b) => a / b;
 console.log(divide(5,2));
